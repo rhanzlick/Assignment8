dfcb4903558eb75dc66c847c09d230135397e818
rhanzlick <rhanzlick90@gmail.com>
2024-04-16 04:18:45 -0400

    TestFailure
    Add a Failing Test

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -23,5 +23,12 @@ namespace UnitTest1
 			CoolClass coolClass;
 			Assert::AreEqual(5, coolClass.Calc(3, 2));
 		}
+
+		TEST_METHOD(TestMethod2)
+		{
+
+			CoolClass coolClass;
+			Assert::AreEqual(1337, coolClass.Calc(3, 2));
+		}
 	};
 }
